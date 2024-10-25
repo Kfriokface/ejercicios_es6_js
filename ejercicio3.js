3183581d7f796ec1f34af753eaeb914389e41166
Alberto Sancho <designalbertosancho@gmail.com>
2024-10-25 20:22:18 +0200

    ejercicio 3-4

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,28 +1,42 @@
-// 3.1 Dado el siguiente array, devuelve un array con sus nombres 
-// utilizando .map().
-// const users = [
-// 	{id: 1, name: 'Abel'},
-// 	{id:2, name: 'Julia'},
-// 	{id:3, name: 'Pedro'},
-// 	{id:4, name: 'Amanda'}
-// ];
+// 3.1 Dado el siguiente array, devuelve un array con sus nombres utilizando .map().
+const users = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
 
-// 3.2 Dado el siguiente array, devuelve una lista que contenga los valores 
-// de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que 
-// empiece por 'A'.
-// const users = [
-// 	{id: 1, name: 'Abel'},
-// 	{id:2, name: 'Julia'},
-// 	{id:3, name: 'Pedro'},
-// 	{id:4, name: 'Amanda'}
-// ];
+const names = users.map(user => user.name);
 
-// 3.3 Dado el siguiente array, devuelve una lista que contenga los valores 
-// de la propiedad .name y añade al valor de .name el string ' (Visitado)' 
-// cuando el valor de la propiedad isVisited = true.
-// const cities = [
-// 	{isVisited:true, name: 'Tokyo'}, 
-// 	{isVisited:false, name: 'Madagascar'},
-// 	{isVisited:true, name: 'Amsterdam'}, 
-// 	{isVisited:false, name: 'Seul'}
-// ];
+console.log('\n--> Ejercicio 3.1');
+console.log(names);
+
+// 3.2 Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que empiece por 'A'.
+const users2 = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+
+const names2 = users2.map(user => {
+  return user.name[0] === 'A' ? 'Anacleto' : user.name;
+});
+
+console.log('\n--> Ejercicio 3.2');
+console.log(names2);
+
+// 3.3 Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name y añade al valor de .name el string ' (Visitado)' cuando el valor de la propiedad isVisited = true.
+const cities = [
+	{isVisited:true, name: 'Tokyo'}, 
+	{isVisited:false, name: 'Madagascar'},
+	{isVisited:true, name: 'Amsterdam'}, 
+	{isVisited:false, name: 'Seul'}
+];
+
+const cityNames = cities.map(city => {
+  return city.isVisited === true ? city.name + ' (Visitado)' : city.name;
+});
+
+console.log('\n--> Ejercicio 3.3');
+console.log(cityNames);
--- ejercicio4.js
+++ ejercicio4.js
@@ -1,32 +1,46 @@
-// 4.1 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
-// con los valores que sean mayor que 18
-// const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+// 4.1 Dado el siguiente array, utiliza .filter() para generar un nuevo array con los valores que sean mayor que 18
+const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
 
-// 4.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
-// con los valores que sean par.
-// const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+const newAges = ages.filter(age => age > 18);
 
-// 4.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
-// con los streamers que tengan el gameMorePlayed = 'League of Legends'.
-// const streamers = [
-// 	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
-// 	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'}, 
-// 	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
-// 	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
-// ];
+console.log('\n--> Ejercicio 4.1');
+console.log(newAges);
 
-// 4.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
-// con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos 
-// usar la funcion .includes() para la comprobación.
-// const streamers = [
-// 	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
-// 	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
-// 	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
-// 	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
-// ];
+// 4.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array con los valores que sean par.
+const newAges2 = ages.filter(age => age % 2 === 0);
 
-// 4.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan 
-// el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion 
-// .includes() para la comprobación.
-// Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando 
-// .age sea mayor que 35.
+console.log('\n--> Ejercicio 4.2');
+console.log(newAges2);
+
+// 4.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array con los streamers que tengan el gameMorePlayed = 'League of Legends'.
+const streamers = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'}, 
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+const lolStreamers = streamers.filter(streamer => streamer.gameMorePlayed === 'League of Legends');
+
+console.log('\n--> Ejercicio 4.3');
+console.log(lolStreamers);
+
+// 4.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array  con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos usar la funcion .includes() para la comprobación.
+
+const uStreamers = streamers.filter(streamer => streamer.name.toLowerCase().includes('u'));
+
+console.log('\n--> Ejercicio 4.4');
+console.log(uStreamers);
+
+// 4.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion .includes() para la comprobación. Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando .age sea mayor que 35.
+
+const oldestStreamers = streamers.filter(streamer => 
+  streamer.gameMorePlayed.includes('Legends')).map(streamer => {
+    if (streamer.age > 35) {
+      return {...streamer, gameMorePlayed: streamer.gameMorePlayed.toUpperCase() };
+    }
+    return streamer;
+  });
+
+  console.log('\n--> Ejercicio 4.5');
+  console.log(oldestStreamers);
